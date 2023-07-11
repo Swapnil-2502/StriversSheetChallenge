9b396f5deafe7422d6979fe8ea46b4d86718ef84
Swapnil-2502 <hajareswapnil.2502@gmail.com>
2023-07-11 11:55:55 +0530

    Next Smaller Element added

---
+++ Stack and Queue Part-II/1. Next Smaller Element.cpp
@@ -0,0 +1,24 @@
+#include<stack>
+vector<int> nextSmallerElement(vector<int> &arr, int n)
+{
+    stack<int> s;
+    vector<int> ans(n,-1);
+
+    for(int i=n-1;i>=0;i--){
+
+        while(!s.empty()){
+            if(s.top() < arr[i]){
+                ans[i] = s.top();
+                break;
+            }
+            s.pop();
+        }
+
+        s.push(arr[i]);
+    }
+    return ans;
+
+    //TC = O(N);
+    //SC = O(N);
+
+}
